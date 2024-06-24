bc231f961cf8b7fca7699f896021e7c09e3fee68
Talgat <abilus17@gmail.com>
2024-06-24 20:45:18 +0500

    task2

--- task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
+++ task2/obj/Debug/net8.0/task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b517884196c7ab7cb835bc17958d334223ccba6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab4dc033758349f910ebb46cf55d7ac7cdd01abb")]
 [assembly: System.Reflection.AssemblyProductAttribute("task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
